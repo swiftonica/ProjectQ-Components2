0a7b85d9a25d677879cf3452f9f2028813f5f7dc
Jeytery <dimaostapchenko@gmail.com>
2023-04-23 14:14:04 +0300

    /

--- Package.swift
+++ Package.swift
@@ -20,7 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ProjectQ-Components2",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"
+        ),
         .testTarget(
             name: "ProjectQ-Components2Tests",
             dependencies: ["ProjectQ-Components2"]),
--- Sources/ProjectQ-Components2/ProjectQ_Components2.swift
+++ Sources/ProjectQ-Components2/ProjectQ_Components2.swift
@@ -1,5 +1,3 @@
-public class ProjectQ_Components2 {}
-
 public typealias Packages = [Package]
 public typealias Tasks = [Task]
 public typealias Components = [Component]
